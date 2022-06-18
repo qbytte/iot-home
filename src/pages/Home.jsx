7928059998ae5efc0be32077aa0ab19171324340
Omar Loyo <omarloyo121@gmail.com>
2022-06-17 23:33:15 -0600

    created home layout and styled it

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import Card from "../components/Card";
+import bulb from "../static/lightbulb-solid.svg";
+import temp from "../static/temperature-half-solid.svg";
+import gitHub from "../static/github-brands.svg";
+import '../styles/Home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <h1>
+        IoT <span>Home</span>
+      </h1>
+      <div className="Cards-Container">
+        <Card
+          img={bulb}
+          title="Light Bulb"
+          desc="Regular bulb controlled by an Arduino and a remote database"
+        />
+        <Card
+          img={temp}
+          title="Temperature"
+          desc="Temperature values obtained from remote database connected to an Arduino board"
+        />
+      </div>
+      <div className="GitHub">
+        <img src={gitHub} alt="Github" />
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
