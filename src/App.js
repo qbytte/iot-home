b1378e594bbed0e8f1d8b5c85f4ba3f9c98e6b0a
Omar Loyo <omarloyo121@gmail.com>
2022-06-17 22:48:18 -0600

    created layout component and styled it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import LightBulb from "./pages/LightBulb";
+import Temps from "./components/Temps";
+import RoomTemp from "./pages/RoomTemp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="bulb" element={<LightBulb />} />
+          <Route path="temps" element={<RoomTemp />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Layout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/Layout.css'
+
+const Layout = ({ children }) => {
+  return (
+    <div className='Layout'>{children}</div>
+  )
+}
+
+export default Layout
