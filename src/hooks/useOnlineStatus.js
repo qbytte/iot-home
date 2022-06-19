fd8daea358b659a1692dc8f5d0580179d54a2a2b
Omar Loyo <omarloyo121@gmail.com>
2022-06-19 00:50:35 -0600

    created hook to determine online status

---
+++ src/hooks/useOnlineStatus.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+export const useOnlineStatus = () => {
+  const [onlineStatus, setOnlineStatus] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener("offline", () => {
+      setOnlineStatus(false);
+    });
+    window.addEventListener("online", () => {
+      setOnlineStatus(true);
+    });
+
+    return () => {
+      window.removeEventListener("offline", () => {
+        setOnlineStatus(false);
+      });
+      window.removeEventListener("online", () => {
+        setOnlineStatus(true);
+      });
+    };
+  }, []);
+
+  return onlineStatus;
+};
